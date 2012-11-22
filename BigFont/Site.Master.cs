0fae7f98ae186b29760fa717c91e213614902225
ShaunLuttin <shaunluttin@gmail.com>
2012-11-22 13:02:29 -0800

    Add a master page so that we can use it as the basis for our other pages, bow that we have multiple pages.

--- BigFont/Site.Master.cs
+++ BigFont/Site.Master.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace BigFont
 {
-    public partial class Default1 : System.Web.UI.Page
+    public partial class Site : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
