c230538924a1ad4dc4b344c8c98d659eb46f3cf9
Shaun Luttin <shaunluttin@gmail.com>
2013-06-20 15:44:12 -0700

    Write javascript to load imgs one at a time instead of all at once, because this will lead, maybe, to a better user experience.

--- BigFont_MVC/assets-10/bigfont/js/bigfont.js
+++ BigFont_MVC/assets-10/bigfont/js/bigfont.js
@@ -9,6 +9,61 @@
     BASE_URL = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port;
     BOOTSTRAP_COLLAPSE_HIDE_DURATION = 195; // approx
 
+    function loadImagesOneByOne() {
+        var imgs, img;
+
+        function deferImg(img) {
+
+            img
+                .attr('data-src', img.attr('src'))
+                .removeAttr('src');
+
+        }
+
+        function loadImg(img) {
+
+            window.console.log('loading ' + img.attr('data-src'));
+
+            img
+                .attr('src', img.attr('data-src'))
+                .removeAttr('data-src');
+        }
+
+        function loadFirstImg() {
+
+            // get the first deferred img
+            img = imgs.first();
+
+            if (imgs.length > 0) {
+
+                // remove it from the array
+                imgs = imgs.not(img);
+
+                // load it
+                loadImg(img);
+
+                // once loaded, load the next img
+                img
+                    .load(function () {
+                        loadFirstImg();
+                    });
+            }
+        }
+
+        // get imgs
+        imgs = $('img');
+
+        // defer loading of all imgs
+        imgs.each(function () {
+            img = $(this);
+            deferImg(img);
+        });
+
+        // load the first img
+        loadFirstImg();
+    }
+
+
     function setupTheAnchorElementNonLinkBehavior() {
 
         // sometimes we want links that do not behave like links
@@ -321,6 +376,8 @@
     }
 
     $(document).ready(function () {
+
+        loadImagesOneByOne();
 
         setupTheAnchorElementNonLinkBehavior();
 
