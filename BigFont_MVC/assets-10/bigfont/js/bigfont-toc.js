82d07856d7ef9541f5b5a70d65e46f072d5f52bd
Shaun Luttin <shaunluttin@gmail.com>
2013-07-03 16:43:12 -0700

    Run the assets optimizer.

--- BigFont_MVC/assets-10/bigfont/js/bigfont-toc.js
+++ BigFont_MVC/assets-10/bigfont/js/bigfont-toc.js
@@ -4,12 +4,14 @@
     /*global $:false, window:false, document:false */
     /*jslint white: true */
 
-    function createListItems()
+    function createTOC()
     {
-
+        window.alert('foo');
     }
 
     $(document).ready(function () {
+
+        createTOC();
 
     });
 
