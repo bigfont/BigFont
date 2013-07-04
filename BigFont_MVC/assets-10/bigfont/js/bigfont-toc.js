205968c1c900e1aa21523cbdbb59c375e1861b60
Shaun Luttin <shaunluttin@gmail.com>
2013-07-03 17:14:07 -0700

    Finish the first draft of bigfont-toc.js

--- BigFont_MVC/assets-10/bigfont/js/bigfont-toc.js
+++ BigFont_MVC/assets-10/bigfont/js/bigfont-toc.js
@@ -4,18 +4,47 @@
     /*global $:false, window:false, document:false */
     /*jslint white: true */
 
-    function createTOC()
-    {
+    function createTOC(ulSelector, iconClass) {
 
+        var sectionID, sectionTitle, ul, li, a, i;
 
+        // get the ul
+        ul = $(ulSelector);
 
+        // iterate the sections
+        $('section').each(function () {
+
+            // get the section id and title
+            sectionID = $(this).attr('id');
+            sectionTitle = $(this)
+                .find('h1', 'h2', 'h3', 'h4')
+                .first()
+                .text();
+
+            // create the li, a, and i
+            li = $('<li/>');
+
+            a = $('<a />', {
+                href: '#' + sectionID,
+                text: sectionTitle
+            });
+
+            i = $('<i/>', {
+                'class': iconClass
+            });
+
+            // mush them together and append to the ul
+            i.prependTo(a);
+            a.appendTo(li);
+            li.appendTo(ul);
+
+        });
     }
 
     $(document).ready(function () {
 
-        createTOC();
+        createTOC('#subnav > ul', 'icon-chevron-right');
 
     });
-
 
 }());
