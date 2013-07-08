dacbf00962c43edd50b4438ed10cdc4412f26aa9
Shaun Luttin <shaunluttin@gmail.com>
2013-07-08 16:08:28 -0700

    Add the bigfont-responsive.css and min.css files to the publish profile.

--- BigFont.MVC/App_Start/BundleConfig.cs
+++ BigFont.MVC/App_Start/BundleConfig.cs
@@ -6,7 +6,7 @@ namespace BigFont.MVC {
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles) {
 
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
 
             #region styles
 
