e33082b623ec16de08c7b081164d008bdb7f3345
Shaun Luttin <shaunluttin@gmail.com>
2013-08-06 20:50:21 -0700

    Use partial view model binding to create the bootstrap project list collapse so that we are more DRY.

--- BigFont.MVC/Controllers/HomeController.cs
+++ BigFont.MVC/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using BigFont.MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace BigFont.MVC.Controllers {
-    public class HomeController : Controller {
+namespace BigFont.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
 
-        protected override void HandleUnknownAction(string actionName) {
+        protected override void HandleUnknownAction(string actionName)
+        {
             try
             {
                 this.View(actionName).ExecuteResult(this.ControllerContext);
@@ -21,45 +25,105 @@ namespace BigFont.MVC.Controllers {
             }
         }
 
-        public ActionResult Index() {
-            return View("Index");
+        public ActionResult Index()
+        {
+
+            // TODO - Implement the Entity Framework to persist this data.
+            #region Prototyping
+
+            List<Project> clients = new List<Project>
+            {
+                new Project {               
+                    PublicName = "The Slope of Kongwa Hill - Alpha",
+                    PublicDescription = "Renovated the existing website to make it both responsive and easy for the client to edit.",
+                    Thumbnail = "portfolio_tsokh",
+                    WebsiteUrl = "tsokh.azurewebsites.net/" },
+                new Project { 
+                    PublicName = "Marill Design", 
+                    PublicDescription = "Constructed this clean, responsive, marketing site from scratch.", 
+                    Thumbnail = "portfolio_marilldesign", 
+                    WebsiteUrl = "www.marilldesign.com" }, 
+                new Project { 
+                    PublicName = "Fernwood Road Cafe", 
+                    PublicDescription = "Provided custom CSS to integrate the cafe's shopping cart into its existing WordPress theme.", 
+                    Thumbnail = "portfolio_fernwoodroadcafe", 
+                    WebsiteUrl = "www.fernwoodcafe.com" }, 
+                new Project { 
+                    PublicName = "Gulf Island Detailing", 
+                    PublicDescription = "Built a clean, responsive, single-page website to market the business.", 
+                    Thumbnail = "portfolio_ssidetailing", 
+                    WebsiteUrl = "www.ssidetailing.com" }, 
+                new Project { 
+                    PublicName = "Dave's Blasting", 
+                    PublicDescription = "Designed and created a responsive, multi-page, media-rich website to market the business.", 
+                    Thumbnail = "portfolio_davesblasting", 
+                    WebsiteUrl = "www.davesblasting.com" }, 
+                new Project { 
+                    PublicName = "Garden Faire", 
+                    PublicDescription = "Modernized the existing by moving its content into a new, responsive content management system.", 
+                    Thumbnail = "portfolio_gardenfaire", 
+                    WebsiteUrl = "www.gardenfaire.ca" }, 
+                new Project { 
+                    PublicName = "Fowl Mood Farm", 
+                    PublicDescription = "Designed our first responsive website and included functionality to allow the owners to update the content.", 
+                    Thumbnail = "portfolio_fowlmoodfarm", 
+                    WebsiteUrl = "www.fowlmoodfarm.com" }, 
+                new Project { 
+                    PublicName = "Nature Works", 
+                    PublicDescription = "Built a refrigeration monitoring system that graphs temperatures and sends warning emails.", 
+                    Thumbnail = "portfolio_natureworks", 
+                    WebsiteUrl = null,
+                    Image = "portfolio_natureworks_large" }
+            };
+            #endregion
+
+            return View("Index", clients);
         }
 
-        public ActionResult Services() {
+        public ActionResult Services()
+        {
             return View();
         }
 
-        public ActionResult Packages() {
+        public ActionResult Packages()
+        {
             return View();
         }
 
-        public ActionResult People() {
+        public ActionResult People()
+        {
             return View();
         }
 
-        public ActionResult Links() {
+        public ActionResult Links()
+        {
             return View();
         }
 
-        public ActionResult Contact() {
+        public ActionResult Contact()
+        {
             return View();
         }
 
-        public ActionResult Checklists() {
+        public ActionResult Checklists()
+        {
             return View();
         }
 
-        public ActionResult Presentations() {
+        public ActionResult Presentations()
+        {
             return View();
         }
 
-        public ActionResult Shop() {
+        public ActionResult Shop()
+        {
 
             return Redirect("https://bigfont-demo-store.myshopify.com/");
 
         }
 
-        public ActionResult SiteMap() {
+        public ActionResult SiteMap()
+        {
             return View();
         }
     }
---
+++ BigFont.MVC/Models/HomeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigFont.MVC.Models
+{
+    public class Project
+    {
+        public string Thumbnail { get; set; }
+        public string PublicName { get; set; }
+        public string PublicDescription { get; set; }
+        public string WebsiteUrl { get; set; }
+        public string Image { get; set; }
+    }
+}
