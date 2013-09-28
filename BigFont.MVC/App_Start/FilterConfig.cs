4c689a04de8294d8bb2c40fdbdf942123d2e82b0
Shaun Luttin <shaunluttin@gmail.com>
2013-09-27 22:56:03 -0700

    Use the RequireHttpsAttribute in GlobalFilters and turn on SSL for IIS Express, because we want to make use of our SSL Certificate for which we paid .

--- BigFont.MVC/App_Start/FilterConfig.cs
+++ BigFont.MVC/App_Start/FilterConfig.cs
@@ -1,10 +1,14 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 
-namespace BigFont.MVC {
-    public class FilterConfig {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+namespace BigFont.MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
