ee5627e75d3b061038a7f6b8f8057a21cef900d8
Shaun Luttin <shaunluttin@gmail.com>
2014-01-01 00:07:45 -0800

    Use brackets to query.

--- BigFont.MVC/Content/google-apis/analytics.js
+++ BigFont.MVC/Content/google-apis/analytics.js
@@ -14,7 +14,7 @@
             // append the query 
             $('<p/>', { text: 'Date Range: ' + results.query['start-date'] + ' to ' + results.query['end-date'] }).appendTo(resultsDiv);
             $('<p/>', { text: 'Profile Name: ' + results.query.profileName }).appendTo(resultsDiv);
-            $('<p/>', { text: 'Total Visits: ' + results.totalsForAllResults[0] }).appendTo(resultsDiv);
+            $('<p/>', { text: 'Total Visits: ' + results.totalsForAllResults['ga:visits'] }).appendTo(resultsDiv);
 
             // append the query results
             ul = $('<ul/>', { 'class': 'unstyled' });
