4f797216ca607cd1f3bf63e2ad46ed5e71bc7227
Shaun Luttin <shaunluttin@gmail.com>
2013-12-31 23:54:43 -0800

    Use the module pattern.

--- BigFont.MVC/Content/google-apis/analytics.js
+++ BigFont.MVC/Content/google-apis/analytics.js
@@ -1,187 +1,193 @@
-﻿
-function printResults(results) {
-    if (results.rows && results.rows.length) {
+﻿/*global $, console, gapi */
 
-        var resultsDiv = $('#ga-results');
-        resultsDiv.empty();
+(function (window) {
 
-        // append the query 
-        $('<p/>', { text: results.query['start-date'] + ' to ' + results.query['end-date'] }).appendTo(resultsDiv);
-        $('<p/>', { text: results.query.profileName });
-        $('<p/>', { text: 'Visits ' + results.totalsForAllResults[0] });
+    function printResults(results) {
+        if (results.rows && results.rows.length) {
+            var resultsDiv, ul, li, j, currentRow, browser, visits, badgeType;
 
-        // append the query results
-        var ul, li;
-        ul = $('<ul/>', { 'class': 'unstyled' });
+            resultsDiv = $('#ga-results');
+            resultsDiv.empty();
 
-        // iterate the rows
-        for (var j = 0; j < results.rows.length; ++j) {
+            // append the query 
+            $('<p/>', { text: results.query['start-date'] + ' to ' + results.query['end-date'] }).appendTo(resultsDiv);
+            $('<p/>', { text: results.query.profileName });
+            $('<p/>', { text: 'Visits ' + results.totalsForAllResults[0] });
 
-            var currentRow, browser, browserVersion, os, osVersion, visits, badgeType;
+            // append the query results
+            ul = $('<ul/>', { 'class': 'unstyled' });
 
-            // skip the first results because it is 'not set'
-            if (j > 0) {
+            // iterate the rows
+            for (j = 0; j < results.rows.length; ++j) {
 
-                // get current row
-                currentRow = results.rows[j];
+                // skip the first results because it is 'not set'
+                if (j > 0) {
 
-                // get cells
-                browser = currentRow[0];
-                visits = currentRow[1];
+                    // get current row
+                    currentRow = results.rows[j];
 
-                // colorize badges
-                if (visits >= 500) {
-                    badgeType = 'important';
+                    // get cells
+                    browser = currentRow[0];
+                    visits = currentRow[1];
+
+                    // colorize badges
+                    if (visits >= 500) {
+                        badgeType = 'important';
+                    } else if (visits >= 250) {
+                        badgeType = 'warning';
+                    } else if (visits >= 50) {
+                        badgeType = 'info';
+                    } else {
+                        badgeType = '';
+                    }
+
+                    // create a list item
+                    li = $('<li/>', { html: (browser + ' <span class="badge badge-' + badgeType + '">' + visits + '</span>') });
+                    ul.append(li);
                 }
-                else if (visits >= 250) {
-                    badgeType = 'warning';
-                }
-                else if (visits >= 50) {
-                    badgeType = 'info';
-                }
-                else {
-                    badgeType = '';
-                }
-
-                // create a list item
-                li = $('<li/>', { html: (browser + ' <span class="badge badge-' + badgeType + '">' + visits + '</span>') });
-                ul.append(li);
             }
-        }
 
-        // append the ul to the document        
-        resultsDiv.append(ul);
-
-    } else {
-        console.log('No results found');
-    }
-}
-
-function handleCoreReportingResults(results) {
-    if (results.error) {
-        console.log('There was an error querying core reporting API: ' + results.message);
-    } else {
-        printResults(results);
-    }
-}
-
-function queryCoreReportingApi(profileId) {
-    console.log('Querying Core Reporting API.');
-
-    // Use the Analytics Service Object to query the Core Reporting API
-    gapi.client.analytics.data.ga.get({
-        'ids': 'ga:' + profileId,
-        'start-date': '2013-01-01',
-        'end-date': '2013-12-31',
-        'metrics': 'ga:visits',
-        'dimensions': 'ga:browser'
-    }).execute(handleCoreReportingResults);
-}
-
-function handleProfiles(results) {
-    if (!results.code) {
-        if (results && results.items && results.items.length) {
-
-            for (var i = 0; i < results.items.length; ++i) {
-
-                // Get the View (Profile) ID
-                var profileID = results.items[i].id;
-
-                console.log('Profile ID: ' + profileID);
-
-                // Step 3. Query the Core Reporting API
-                queryCoreReportingApi(profileID);
-            }
+            // append the ul to the document        
+            resultsDiv.append(ul);
 
         } else {
-            console.log('No views (profiles) found for this user.');
+            console.log('No results found');
         }
-    } else {
-        console.log('There was an error querying views (profiles): ' + results.message);
     }
-}
 
-function queryProfiles(accountId, webpropertyId) {
-    console.log('Querying Views (Profiles).');
-
-    // Get a list of all Views (Profiles) for the first Web Property of the first Account
-    gapi.client.analytics.management.profiles.list({
-        'accountId': accountId,
-        'webPropertyId': webpropertyId
-    }).execute(handleProfiles);
-}
-
-function handleWebproperties(results) {
-    if (!results.code) {
-        if (results && results.items && results.items.length) {
-
-            for (var i = 0; i < results.items.length; ++i) {
-
-                // Get the Google Analytics account
-                var accountId = results.items[i].accountId
-
-                // Get the Web Property ID
-                var webPropertyId = results.items[i].id;
-
-                console.log('Account ID: ' + accountId);
-                console.log('Property ID: ' + webPropertyId);
-
-                // Query only the first web property
-                if (webPropertyId === 'UA-36302929-1') {
-                    // Query for Views (Profiles)
-                    queryProfiles(accountId, webPropertyId);
-                }
-            }
-
+    function handleCoreReportingResults(results) {
+        if (results.error) {
+            console.log('There was an error querying core reporting API: ' + results.message);
         } else {
-            console.log('No webproperties found for this user.');
+            printResults(results);
         }
-    } else {
-        console.log('There was an error querying webproperties: ' + results.message);
     }
-}
 
-function queryWebproperties(accountId) {
-    console.log('Querying Webproperties.');
+    function queryCoreReportingApi(profileId) {
+        console.log('Querying Core Reporting API.');
 
-    // Get a list of all the Web Properties for the account
-    gapi.client.analytics.management.webproperties.list({ 'accountId': accountId }).execute(handleWebproperties);
-}
+        // Use the Analytics Service Object to query the Core Reporting API
+        gapi.client.analytics.data.ga.get({
+            'ids': 'ga:' + profileId,
+            'start-date': '2013-01-01',
+            'end-date': '2013-12-31',
+            'metrics': 'ga:visits',
+            'dimensions': 'ga:browser'
+        }).execute(handleCoreReportingResults);
+    }
 
-function handleAccounts(results) {
-    if (!results.code) {
-        if (results && results.items && results.items.length) {
+    function handleProfiles(results) {
 
-            for (var i = 0; i < results.items.length; ++i) {
+        var i, profileID;
 
-                // Get the Google Analytics account
-                var accountId = results.items[i].id
+        if (!results.code) {
+            if (results && results.items && results.items.length) {
 
-                console.log('Account ID: ' + accountId);
+                for (i = 0; i < results.items.length; ++i) {
 
-                // query only the BigFont.ca account
-                // because we have a rate limit.
-                if (accountId === '36302929') {
-                    // Query for Web Properties
-                    queryWebproperties(accountId);
+                    // Get the View (Profile) ID
+                    profileID = results.items[i].id;
+
+                    console.log('Profile ID: ' + profileID);
+
+                    // Step 3. Query the Core Reporting API
+                    queryCoreReportingApi(profileID);
                 }
+
+            } else {
+                console.log('No views (profiles) found for this user.');
             }
-
         } else {
-            console.log('No accounts found for this user.')
+            console.log('There was an error querying views (profiles): ' + results.message);
         }
-    } else {
-        console.log('There was an error querying accounts: ' + results.message);
     }
-}
 
-function queryAccounts() {
-    console.log('Querying Accounts.');
+    function queryProfiles(accountId, webpropertyId) {
+        console.log('Querying Views (Profiles).');
 
-    // Get a list of all Google Analytics accounts for this user
-    gapi.client.analytics.management.accounts.list().execute(handleAccounts);
-}
+        // Get a list of all Views (Profiles) for the first Web Property of the first Account
+        gapi.client.analytics.management.profiles.list({
+            'accountId': accountId,
+            'webPropertyId': webpropertyId
+        }).execute(handleProfiles);
+    }
 
-function makeApiCall() {
-    queryAccounts();
-}
+    function handleWebproperties(results) {
+
+        var i, accountId, webPropertyId;
+
+        if (!results.code) {
+            if (results && results.items && results.items.length) {
+
+                for (i = 0; i < results.items.length; ++i) {
+
+                    // Get the Google Analytics account
+                    accountId = results.items[i].accountId;
+
+                    // Get the Web Property ID
+                    webPropertyId = results.items[i].id;
+
+                    console.log('Account ID: ' + accountId);
+                    console.log('Property ID: ' + webPropertyId);
+
+                    // Query only the first web property
+                    if (webPropertyId === 'UA-36302929-1') {
+                        // Query for Views (Profiles)
+                        queryProfiles(accountId, webPropertyId);
+                    }
+                }
+
+            } else {
+                console.log('No webproperties found for this user.');
+            }
+        } else {
+            console.log('There was an error querying webproperties: ' + results.message);
+        }
+    }
+
+    function queryWebproperties(accountId) {
+        console.log('Querying Webproperties.');
+
+        // Get a list of all the Web Properties for the account
+        gapi.client.analytics.management.webproperties.list({ 'accountId': accountId }).execute(handleWebproperties);
+    }
+
+    function handleAccounts(results) {
+        if (!results.code) {
+            if (results && results.items && results.items.length) {
+
+                for (var i = 0; i < results.items.length; ++i) {
+
+                    // Get the Google Analytics account
+                    var accountId = results.items[i].id
+
+                    console.log('Account ID: ' + accountId);
+
+                    // query only the BigFont.ca account
+                    // because we have a rate limit.
+                    if (accountId === '36302929') {
+                        // Query for Web Properties
+                        queryWebproperties(accountId);
+                    }
+                }
+
+            } else {
+                console.log('No accounts found for this user.')
+            }
+        } else {
+            console.log('There was an error querying accounts: ' + results.message);
+        }
+    }
+
+    function queryAccounts() {
+        console.log('Querying Accounts.');
+
+        // Get a list of all Google Analytics accounts for this user
+        gapi.client.analytics.management.accounts.list().execute(handleAccounts);
+    }
+
+    window.makeApiCall = function () {
+        queryAccounts();
+    }
+
+}());
