1f97c3b98aee48be72a09ce6f17bfecdd4f7fc84
Shaun Luttin <shaunluttin@gmail.com>
2014-01-01 13:48:29 -0800

    Refactor.

---
+++ BigFont.MVC/Filters/SwitchToHttp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigFont.MVC.Filters
+{   
+    public class SwitchToHttp : ActionFilterAttribute
+    {
+        private Uri SwitchUriFromHttpsToHttp(Uri uri)
+        {
+            string scheme;
+            string host;
+            int port;
+            string pathAndQuery;
+            UriBuilder builder;
+
+            scheme = uri.Scheme.ToLower();
+            host = uri.Host.ToLower();
+            port = uri.Port;
+            pathAndQuery = uri.PathAndQuery.ToLower();
+
+            if (scheme.Equals("https"))
+            {
+                scheme = scheme.Replace("s", string.Empty);
+            }
+
+            builder = new UriBuilder(scheme, host, port) { Path = pathAndQuery };
+
+            return builder.Uri;
+        }
+        private bool IsHttpUri(Uri uri)
+        {
+            return uri.Scheme.ToLower().Equals("http");
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Uri uri;
+            uri = filterContext.HttpContext.Request.Url;
+
+            if (!IsHttpUri(uri))
+            {
+                uri = SwitchUriFromHttpsToHttp(uri);
+                filterContext.HttpContext.Response.Redirect(uri.ToString());
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ BigFont.MVC/Filters/SwitchToHttps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigFont.MVC.Filters
+{   
+    public class SwitchToHttps : ActionFilterAttribute
+    {
+        private Uri SwitchUriFromHttpToHttps(Uri uri)
+        {
+            string scheme;
+            string host;
+            int port;
+            string pathAndQuery;
+            UriBuilder builder;
+
+            scheme = uri.Scheme.ToLower();
+            host = uri.Host.ToLower();
+            port = uri.Port;
+            pathAndQuery = uri.PathAndQuery.ToLower();
+
+            if (scheme.Equals("http"))
+            {
+                scheme += "s";
+            }
+
+            builder = new UriBuilder(scheme, host, port) { Path = pathAndQuery };
+
+            return builder.Uri;
+        }
+        private bool IsHttpsUri(Uri uri)
+        {
+            return uri.Scheme.ToLower().Equals("https");
+        }
+        private bool IsLocalUri(Uri uri)
+        {
+            return uri.Host.ToLower().Contains("localhost");
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Uri uri;
+            uri = filterContext.HttpContext.Request.Url;
+
+            if (!IsHttpsUri(uri) && !IsLocalUri(uri))
+            {
+                uri = SwitchUriFromHttpToHttps(uri);
+                filterContext.HttpContext.Response.Redirect(uri.ToString());
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
