31b874cced18cc7706a3c362a02df1137f4f838b
Shaun Luttin <shaunluttin@gmail.com>
2014-01-03 13:47:23 -0800

    Bundle the Google Analytics api scripts.

--- BigFont.MVC/App_Start/BundleConfig.cs
+++ BigFont.MVC/App_Start/BundleConfig.cs
@@ -1,23 +1,26 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace BigFont.MVC {
-    public class BundleConfig {
+namespace BigFont.MVC
+{
+    public class BundleConfig
+    {
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
-        public static void RegisterBundles(BundleCollection bundles) {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
 
             BundleTable.EnableOptimizations = true;
 
             #region styles
 
             bundles.Add(new StyleBundle("~/crispy").Include(
-                "~/Content/bootstrap/css/bootstrap.css",                
+                "~/Content/bootstrap/css/bootstrap.css",
                 "~/Content/font-awesome/css/font-awesome.css",
                 "~/Content/bigfont/css/bigfont.css",
                 // bring in responsive to cascade over the non-responsive css
                 "~/Content/bootstrap/css/responsive.css",
                 "~/Content/bigfont/css/bigfont-responsive.css",
-                "~/Content/bigfont/css/bigfont-print.css"                
+                "~/Content/bigfont/css/bigfont-print.css"
                 ));
 
             #endregion
@@ -28,10 +31,15 @@ namespace BigFont.MVC {
                 "~/Scripts/jquery-{version}.js",
                 "~/Scripts/jquery.validate.js",
                 "~/Scripts/jquery.cookie.js",
-                "~/Scripts/bootstrap.js",                
+                "~/Scripts/bootstrap.js",
                 "~/Scripts/bigfont.js",
                 "~/Scripts/bigfont-toc.js"
                 ));
+
+            bundles.Add(new ScriptBundle("~/ganalytics").Include(
+                "~/Scripts/bigfont-gapi-analytics-auth.js",
+                "~/Scripts/bigfont-gapi-analytics.js"
+            ));
 
             #endregion
 
