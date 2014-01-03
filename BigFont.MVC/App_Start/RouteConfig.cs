870d9f390ea90267dab9e5a5e42fdf74a31a88b1
Shaun Luttin <shaunluttin@gmail.com>
2014-01-02 17:41:08 -0800

    Re-add the ignoring of resources.

--- BigFont.MVC/App_Start/RouteConfig.cs
+++ BigFont.MVC/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace BigFont.MVC
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.IgnoreRoute("Images/{*relpath}");
 
             routes.RouteExistingFiles = true;
