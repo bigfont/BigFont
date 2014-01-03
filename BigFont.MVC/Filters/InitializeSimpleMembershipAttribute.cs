1b7ea9a877e10eb1c5de0a6d283c8e733b4f4b94
Shaun Luttin <shaunluttin@gmail.com>
2014-01-03 14:53:13 -0800

    Update the connection string name.

--- BigFont.MVC/Filters/InitializeSimpleMembershipAttribute.cs
+++ BigFont.MVC/Filters/InitializeSimpleMembershipAttribute.cs
@@ -30,10 +30,7 @@ namespace BigFont.MVC.Filters {
                         }
                     }
 
-
-
-                    ////connString = Environment.GetEnvironmentVariable("SQLAZURECONNSTR_myConnStringName");
-                    WebSecurity.InitializeDatabaseConnection("SQLAZURECONNSTR_bigfont_db", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    WebSecurity.InitializeDatabaseConnection("bigfont_db", "UserProfile", "UserId", "UserName", autoCreateTables: true);
                 }
                 catch (Exception ex) {
                     throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
