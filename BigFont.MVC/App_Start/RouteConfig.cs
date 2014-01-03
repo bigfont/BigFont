f18277e1ce0a78e75165394a2ed1c84521b4e5ab
Shaun Luttin <shaunluttin@gmail.com>
2014-01-02 16:49:11 -0800

    Add a route to handle /account/login.

--- BigFont.MVC/App_Start/RouteConfig.cs
+++ BigFont.MVC/App_Start/RouteConfig.cs
@@ -5,19 +5,29 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace BigFont.MVC {
-    public class RouteConfig {
-        public static void RegisterRoutes(RouteCollection routes) {
+namespace BigFont.MVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
 
             routes.RouteExistingFiles = true; // allow routes with the same names as directories e.g. Services
 
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
+                name: "Home",
                 url: "{action}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
+
         }
     }
 }
