23ba914f73a4747d0cdfd321f05e3a98b0607cba
Shaun Luttin <shaunluttin@gmail.com>
2014-01-13 11:35:07 -0800

    Turn custom errors back on because we have completed debugging.

--- BigFont.MVC/Services/IAppSettingsService.cs
+++ BigFont.MVC/Services/IAppSettingsService.cs
@@ -5,6 +5,9 @@ using System.Web;
 
 namespace BigFont.MVC.Services
 {
+    /// <summary>
+    /// These keys are stored in Windows Azure > Websites > [Website Name] > Configure > app settings
+    /// </summary>
     public interface IAppSettingsService
     {
         string GaServicePrivateKeyFilePath { get; }
