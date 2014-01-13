2101a53408542ef7ce7a8a284f0c98bad713b7fe
Shaun Luttin <shaunluttin@gmail.com>
2014-01-13 11:03:58 -0800

    Add new files to git, because otherwise they will not publish.

---
+++ BigFont.MVC/Services/AppSettingsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigFont.MVC.Services
+{
+    public class AppSettingsService : IAppSettingsService
+    {
+        private string GetAppSettingsValue(string key)
+        {
+            return System.Configuration.ConfigurationManager.AppSettings[key].ToString();
+        }
+
+        public string GaServicePrivateKeyFilePath { get { return GetAppSettingsValue("GaServicePrivateKeyFilePath"); } }
+        public string GaServiceAccountEmail { get { return GetAppSettingsValue("GaServiceAccountEmail"); } }
+    }
+}
---
+++ BigFont.MVC/Services/IAppSettingsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigFont.MVC.Services
+{
+    public interface IAppSettingsService
+    {
+        string GaServicePrivateKeyFilePath { get; }
+        string GaServiceAccountEmail { get; }
+    }
+}
