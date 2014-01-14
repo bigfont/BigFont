7253709a70543777c16aeb52c7d2ee7bba1fcbd9
Shaun Luttin <shaunluttin@gmail.com>
2014-01-13 18:58:35 -0800

    Upgrade to .net 4.5.1 from 4.5 because it's more recent.

---
+++ BigFont.HtmlHelpers/DemoLabelHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BigFont.HtmlHelpers
+{
+    public class DemoLabelHelper
+    {
+        public static string Label(string target, string text)
+        {
+            return String.Format("<label for='{0}'>{1}</label>", target, text);
+        }
+    }
+}
