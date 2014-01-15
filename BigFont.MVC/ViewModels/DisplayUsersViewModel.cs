87219343c055cef9042170f519aac8cae6677255
Shaun Luttin <shaunluttin@gmail.com>
2014-01-14 20:33:30 -0800

    Add new files to the git repo.

---
+++ BigFont.MVC/ViewModels/DisplayUsersViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using DotNetOpenAuth.OAuth.Messages;
+
+namespace BigFont.MVC.ViewModels
+{
+    public class DisplayUsersViewModel
+    {
+        public Role[] Roles { get; set; }
+
+        public DisplayUsersViewModel()
+        {
+            Roles = new Role[] {};
+        }
+
+        public class Role
+        {
+            public Role()
+            {
+                Users = new string[] {};
+            }
+            public string Name { get; set; }
+            public string[] Users { get; set; }
+        }
+    }
+}
