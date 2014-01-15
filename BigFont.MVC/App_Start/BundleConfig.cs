7c338034e40d7aefade0a8386bb34b67af9561d8
Shaun Luttin <shaunluttin@gmail.com>
2014-01-15 02:48:34 -0800

    Update the navbar.

--- BigFont.MVC/App_Start/BundleConfig.cs
+++ BigFont.MVC/App_Start/BundleConfig.cs
@@ -12,13 +12,13 @@ namespace BigFont.MVC
             #region styles
 
             bundles.Add(new StyleBundle("~/crispy").Include(
-                "~/Content/bootstrap/css/bootstrap.css",
-                "~/Content/font-awesome/css/font-awesome.css",
-                "~/Content/bigfont/css/bigfont.css",
+                "~/Content/bootstrap/css/bootstrap.css"
+                ,"~/Content/font-awesome/css/font-awesome.css"
+                ,"~/Content/bigfont/css/bigfont.css"
                 // bring in responsive to cascade over the non-responsive css
-                "~/Content/bootstrap/css/responsive.css",
-                "~/Content/bigfont/css/bigfont-responsive.css",
-                "~/Content/bigfont/css/bigfont-print.css"
+                ,"~/Content/bootstrap/css/responsive.css"
+                ,"~/Content/bigfont/css/bigfont-responsive.css"
+                ,"~/Content/bigfont/css/bigfont-print.css"
                 ));
 
             #endregion
