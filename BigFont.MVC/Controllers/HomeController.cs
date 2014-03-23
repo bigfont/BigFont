81de1808b44d11580caff22b28765677f3128e14
Shaun Luttin <shaunluttin@gmail.com>
2014-03-23 11:58:34 -0700

    Add cakelove to portfolio.Delete all thumbnail portfolio images.

--- BigFont.MVC/Controllers/HomeController.cs
+++ BigFont.MVC/Controllers/HomeController.cs
@@ -53,9 +53,14 @@ namespace BigFont.MVC.Controllers
             {
                 new Project
                 {
+                    PublicName = "Cake Love Instructors Registration",
+                    PublicDescription = "Created an online application form for potential Cake Love instructors.",
+                    WebsiteUrl = "cakelove.azurewebsites.net"
+                },
+                new Project
+                {
                     PublicName = "David Jackson Gallery",
                     PublicDescription = "Repaired the broken WordPress website after it broke due to improper upgrades.",
-                    Thumbnail = "portfolio_davidJacksonGallery",
                     WebsiteUrl = "www.davidjacksongallery.com/"
                 },
                 new Project
@@ -63,7 +68,6 @@ namespace BigFont.MVC.Controllers
                     PublicName = "Salt Spring Conjuring Club",
                     PublicDescription =
                         "AngularJS and Bootstrap on the client with SQL Server, MVC.NET and C# hosted in Windows Azure.",
-                    Thumbnail = "portfolio_conjuringclub",
                     WebsiteUrl = "www.1in52.ca"
                 },
                 new Project
@@ -71,21 +75,18 @@ namespace BigFont.MVC.Controllers
                     PublicName = "Arc Economics",
                     PublicDescription =
                         "Integrated DropBox into a clean, responsive, and custom content management system.",
-                    Thumbnail = "portfolio_arceconomics",
                     WebsiteUrl = "www.arceconomics.com"
                 },
                 new Project
                 {
                     PublicName = "Sweet Water Revolver",
                     PublicDescription = "The under-construction Sweet Water Revolver website.",
-                    Thumbnail = "portfolio_sweetwater",
                     WebsiteUrl = "sweetwater.azurewebsites.net"
                 },
                 new Project
                 {
                     PublicName = "Pet Portraitures",
                     PublicDescription = "The under-construction Pet Portraitures website.",
-                    Thumbnail = "portfolio_pets",
                     WebsiteUrl = "petsite.azurewebsites.net"
                 },
                 new Project
@@ -93,14 +94,12 @@ namespace BigFont.MVC.Controllers
                     PublicName = "Muddling Thru",
                     PublicDescription =
                         "Created a simple, responsive blog to answer client questions and to explore computer programming.",
-                    Thumbnail = "portfolio_muddlingthru",
                     WebsiteUrl = "www.muddlingthru.ca"
                 },
                 new Project
                 {
                     PublicName = "The Castaway Endurance Run",
                     PublicDescription = "Uploaded Garmin GPS data to create a printable, responsive map of the race.",
-                    Thumbnail = "portfolio_castaway",
                     WebsiteUrl = "castaway.azurewebsites.net/"
                 },
                 new Project
@@ -108,14 +107,12 @@ namespace BigFont.MVC.Controllers
                     PublicName = "The Slope of Kongwa Hill",
                     PublicDescription =
                         "Renovated the existing website to make it both responsive and easy for the client to edit.",
-                    Thumbnail = "portfolio_tsokh",
                     WebsiteUrl = "tsokh.azurewebsites.net/"
                 },
                 new Project
                 {
                     PublicName = "Marill Design",
                     PublicDescription = "Constructed this clean, responsive, marketing site from scratch.",
-                    Thumbnail = "portfolio_marilldesign",
                     WebsiteUrl = "www.marilldesign.com"
                 },
                 new Project
@@ -123,14 +120,12 @@ namespace BigFont.MVC.Controllers
                     PublicName = "Fernwood Road Cafe",
                     PublicDescription =
                         "Provided custom CSS to integrate the cafe's shopping cart into its existing WordPress theme.",
-                    Thumbnail = "portfolio_fernwoodroadcafe",
                     WebsiteUrl = "www.fernwoodcafe.com"
                 },
                 new Project
                 {
                     PublicName = "Gulf Island Detailing",
                     PublicDescription = "Built a clean, responsive, single-page website to market the business.",
-                    Thumbnail = "portfolio_ssidetailing",
                     WebsiteUrl = "www.ssidetailing.com"
                 },
                 new Project
@@ -138,7 +133,6 @@ namespace BigFont.MVC.Controllers
                     PublicName = "Dave's Blasting",
                     PublicDescription =
                         "Designed and created a responsive, multi-page, media-rich website to market the business.",
-                    Thumbnail = "portfolio_davesblasting",
                     WebsiteUrl = "www.davesblasting.com"
                 },
                 new Project
@@ -146,7 +140,6 @@ namespace BigFont.MVC.Controllers
                     PublicName = "Garden Faire",
                     PublicDescription =
                         "Modernized the existing by moving its content into a new, responsive content management system.",
-                    Thumbnail = "portfolio_gardenfaire",
                     WebsiteUrl = "www.gardenfaire.ca"
                 },
                 new Project
@@ -154,7 +147,6 @@ namespace BigFont.MVC.Controllers
                     PublicName = "Fowl Mood Farm",
                     PublicDescription =
                         "Designed our first responsive website and included functionality to allow the owners to update the content.",
-                    Thumbnail = "portfolio_fowlmoodfarm",
                     WebsiteUrl = "www.fowlmoodfarm.com"
                 }
                 // disclude nature works, 
