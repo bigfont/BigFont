21ef48a3434e417895aca9c005a2915fc4b1ae35
Shaun Luttin <shaunluttin@gmail.com>
2014-04-20 15:24:21 -0700

    Successfully display miniBlog content in BigFont main content area.

---
+++ BigFont.MVC/Areas/MiniBlog/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigFont.MVC.Areas.MiniBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        //
+        // GET: /MiniBlog/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
