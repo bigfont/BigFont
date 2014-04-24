b7fe2d34a758574e883569f5b17190ad54cabd52
Shaun Luttin <shaunluttin@gmail.com>
2014-04-24 12:04:11 -0700

    Add a PowerPoint for XP.

--- BigFont.MVC/ViewModels/PresentationViewModel.cs
+++ BigFont.MVC/ViewModels/PresentationViewModel.cs
@@ -7,14 +7,15 @@ namespace BigFont.MVC.ViewModels
 {
     public class PresentationViewModel
     {
-        public PresentationViewModel(string month, string year, string title, string intro, string presenter, string powerPointLink, string posterName)
+        public PresentationViewModel(string month, string year, string title, string intro, string presenter, string resourceLink, string resourceName, string posterName)
         {
             this.Month = month;
             this.Year = year;
             this.Intro = intro;
             this.Title = title;
             this.Presenter = presenter;
-            this.PowerPointLink = powerPointLink;
+            this.ResourceLink = resourceLink;
+            this.ResourceName = resourceName;
             this.PosterHref = string.Format("/Permalinks/{0}.pdf", posterName);
             this.PosterSrc = string.Format("/Images/{0}.png", posterName);
         }
@@ -23,7 +24,8 @@ namespace BigFont.MVC.ViewModels
         public string Title { get; private set; }
         public string Intro { get; private set; }
         public string Presenter { get; private set; }
-        public string PowerPointLink { get; private set; }
+        public string ResourceLink { get; private set; }        
+        public string ResourceName { get; set; }
         public string PosterHref { get; private set; }
         public string PosterSrc { get; private set; }
     }
