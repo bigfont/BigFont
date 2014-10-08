1a70cc8a39260076fab65d495708fdb9295421d9
Shaun Luttin <shaunluttin@gmail.com>
2014-10-08 12:08:14 -0700

    Add legacy routes.

--- BigFont.MVC/App_Start/RouteConfig.cs
+++ BigFont.MVC/App_Start/RouteConfig.cs
@@ -11,13 +11,26 @@ namespace BigFont.MVC
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            // do not route these paths
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("Permalinks/{*relpath}");
+            routes.IgnoreRoute("Scripts/{*relpath}");
+            routes.IgnoreRoute("Images/{*relpath}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            // route all other paths, even if they exist on disk
+            routes.RouteExistingFiles = true;
+
+            routes.MapRoute("ActionOnly", "{action}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute("ControllerAction", "{controller}/{action}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute("Default", "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
         }
     }
 }
