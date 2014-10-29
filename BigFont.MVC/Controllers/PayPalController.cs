92a105cc4776b9eba136ab426af5ab85722085b9
Shaun Luttin <shaun@bigfont.ca>
2014-10-29 15:34:09 -0700

    Add paypal views.

---
+++ BigFont.MVC/Controllers/PayPalController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigFont.MVC.Controllers
+{
+    public class PayPalController : Controller
+    {
+        // GET: PayPal
+        public ActionResult Cancel()
+        {
+            return View();
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
