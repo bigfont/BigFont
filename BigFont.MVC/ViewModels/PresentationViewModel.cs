e88d502158c5b57c987519d4fc24c49600eb0983
Shaun Luttin <shaun@bigfont.ca>
2014-10-29 16:49:31 -0700

    Add completed not for presentations.

--- BigFont.MVC/ViewModels/PresentationViewModel.cs
+++ BigFont.MVC/ViewModels/PresentationViewModel.cs
@@ -7,7 +7,7 @@ namespace BigFont.MVC.ViewModels
 {
     public class PresentationViewModel    
     {
-        public PresentationViewModel(string month, string year, string title, string intro, string presenter, string resourceLink, string resourceName, string posterName)
+        public PresentationViewModel(string month, int year, string title, string intro, string presenter, string resourceLink, string resourceName, string posterName, bool completed = false)
         {
             this.Month = month;
             this.Year = year;
@@ -17,14 +17,16 @@ namespace BigFont.MVC.ViewModels
             this.ResourceLink = resourceLink;
             this.ResourceName = resourceName;
             this.PosterSrc = string.Format("/Images/{0}.png", posterName);
+            this.Completed = completed;
         }
         public string Month { get; private set; }
-        public string Year { get; private set; }
+        public int Year { get; private set; }
         public string Title { get; private set; }
         public string Intro { get; private set; }
         public string Presenter { get; private set; }
         public string ResourceLink { get; private set; }        
         public string ResourceName { get; set; }
         public string PosterSrc { get; private set; }
+        public bool Completed { get; set; }
     }
 }
